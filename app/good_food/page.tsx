a5e0e16314a0d6674ce6c2d5a3185361d1820912
matt-the-thew <63616471+matt-the-thew@users.noreply.github.com>
2026-01-10 16:09:12 -0800

    start setting up supabase

---
+++ app/good_food/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import * as React from "react";
+import { Heading } from "@chakra-ui/react";
+
+export default function GoodFood() {
+  return (
+    <>
+      <Heading>mmm food</Heading>
+    </>
+  );
+}
