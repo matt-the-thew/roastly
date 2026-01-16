0f38aee5cc3755e77e153911263a3b8bbcd482d8
matt-the-thew <63616471+matt-the-thew@users.noreply.github.com>
2026-01-15 17:43:04 -0800

    "changed logged_in to no longer redirect"

--- app/dashboard/logged_in/page.tsx
+++ app/dashboard/logged_in/page.tsx
@@ -1,11 +1,7 @@
 import * as React from "react";
 import { Heading, Container, Image } from "@chakra-ui/react";
-import { redirect } from "next/navigation";
-import { createClient } from "@/lib/supabase/client";
 
 export default async function LoggedIn() {
-  const supabase = createClient();
-
   return (
     <>
       <Container w="20rem" mx="auto" mt="30vh">
