4c4d421eafd347f7b77b0ff11c0c1cf0a666b2e2
Matthew Morton <63616471+matt-the-thew@users.noreply.github.com>
2026-05-05 17:47:47 -0700

    Documentation for /auth/confirm

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -3,10 +3,16 @@ import { type NextRequest } from "next/server";
 import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 
-/*Searches parameters for either code or token hash,
-which varies based on Supabase version. Based on what it finds,
-calls the respectively correct function and handles errors.
-*/
+/**
+ * Searches request parameters for either a code or token hash,
+ * which vaires based on Supabase version. Dynamically calls either
+ * (`supabase.auth.exchangeCodeForSession`)[https://supabase.com/docs/reference/javascript/auth-setsession],
+ * or
+ * (`supabase.auth.verifyOtp`)[https://supabase.com/docs/guides/auth/auth-email-passwordless?queryGroups=language&language=js#step-2-verify-the-otp-to-create-a-session].
+ *
+ * @param request - Request to parse auth parameters from.
+ * @returns {Error} if auth session can't be established from `request`.
+ */
 async function verifyToken(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
@@ -17,9 +23,11 @@ async function verifyToken(request: NextRequest) {
   if (code) {
     const { error } = await supabase!.auth.exchangeCodeForSession(code);
     if (!error) return;
-    throw new Error(
-      `[AUTH/CONFIRM]: error exchanging code for session: ${error.message}`,
-    );
+    else {
+      throw new Error(
+        `[AUTH/CONFIRM]: error exchanging code for session: ${error.message}`,
+      );
+    }
   } else if (token_hash && type) {
     const { error } = await supabase!.auth.verifyOtp({
       token_hash: token_hash,
@@ -32,13 +40,20 @@ async function verifyToken(request: NextRequest) {
   }
 }
 
+/**
+ * Handles email confirmation request, using {@link verifyToken}.
+ * If `verifyToken` throws an error, redirects to `/auth/login`.
+ * If logged in user has no profile, sends them to onboarding.
+ * Otherwise, directs user to the homepage.
+ * @param request - Email confirmation request.
+ */
 export async function GET(request: NextRequest) {
   try {
     await verifyToken(request);
   } catch (err: unknown) {
     if (err instanceof Error) {
       console.error(`[AUTH/CONFIRM]: Error verifying token: ${err.message}`);
-      redirect("/auth/auth-code-error");
+      redirect("/auth/login");
     }
   }
 
