e8fe9dae8a2bee9b29cace5b03f4d0ddce3da23d
Matthew Morton <63616471+matt-the-thew@users.noreply.github.com>
2026-06-11 13:18:02 -0700

    FEAT: Add playwright + initial tests
    1.) Added playwright package.
    2.) Wrote first couple of tests in test/e2e

---
+++ tests/e2e/proxy.test.ts
@@ -0,0 +1,14 @@
+import { test, expect } from "playwright/test";
+
+test("Unauthorized user redirected to login", async ({ page }) => {
+  await page.goto("http://localhost:3000/dashboard/homepage");
+  await expect(page).toHaveURL("http://localhost:3000/auth/login");
+});
+
+test("Valid login submission works", async ({ page }) => {
+  await page.goto("http://localhost:3000/auth/login");
+
+  await page.fill("input[name='email'", "hello@world.com");
+  await page.fill("input[name='password'", "developer");
+  await page.click("button[type='submit'");
+});
