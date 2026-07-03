ed559909643a1c059383f546864de6b2da94f437
Matthew Morton <63616471+matt-the-thew@users.noreply.github.com>
2026-07-03 13:10:53 -0700

    Disable build_commands for three Vercel envs
    Previous build_commands made Vercel not deploy on pushes to "dev", and
    only on "staging" and "prod". Now, Vercel builds to preview from dev,
    and relatively named environments from staging and prod.

--- build_commands.sh
+++ build_commands.sh
@@ -1,3 +1,8 @@
 # Vercel specific build step commands
+# Do not deploy pushes to "dev"
 # Vercel's system continues with the build if 1, and cancels with 0
-if [ "$VERCEL_GIT_COMMIT_REF" = "staging" ]; then exit 1; else exit 0; fi
+# if [ "$VERCEL_GIT_COMMIT_REF" = "dev" ]; then exit 0; else exit 1; fi
+#
+# --- DISABLED STEP 7-3-26 ---
+# DevOps overhaul; Vercel deploys dev to preview,
+# staging to staging.roastly.dev, and prod to www.roastly.dev
